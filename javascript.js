6e98fe9d5ee0baddf6f6d80557290c9144672528
Hoody98 <ben.hood98@aol.com>
2023-11-08 13:33:37 +0000

    Fixed typo(1 to i, line6)

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ const containerDiv = document.querySelector("#container");
 function makeRows(rows, columns) {
     containerDiv.style.setProperty("--grid-rows", rows);
     containerDiv.style.setProperty("--grid-columns", columns);
-    for (1=0; i < (rows * columns); i++) {
+    for (i = 0; i < (rows * columns); i++) {
         let square = document.createElement("div");
         square.innerText = (i + 1); //remove later, provides labelling of grid/clarity
         containerDiv.appendChild(square).className = "grid-item";
