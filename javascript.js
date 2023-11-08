83cfaa5fadf110cc6d1aa8a4c52b5aa2b67c4503
Hoody98 <ben.hood98@aol.com>
2023-11-08 14:06:38 +0000

    added mouse hover eventlistener, fills square to black

--- javascript.js
+++ javascript.js
@@ -5,8 +5,11 @@ function makeRows(rows, columns) {
     containerDiv.style.setProperty("--grid-columns", columns);
     for (i = 0; i < (rows * columns); i++) {
         let square = document.createElement("div");
-        square.innerText = (i + 1); //remove later, provides labelling of grid/clarity
         containerDiv.appendChild(square).className = "grid-item";
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+            console.log(square); //For code execution visibiity
+        })
     }
 }
 
